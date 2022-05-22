17005332b2bbc0b6d69000858cdedfe9633ea344
munasinghetd@gmail.com <d@n@1993>
2022-05-22 16:07:33 +0530

    sector import

--- app/Http/Controllers/Admin/EducationsController.php
+++ app/Http/Controllers/Admin/EducationsController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Education;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use phpDocumentor\Reflection\DocBlock\Tags\Return_;
 use Rap2hpoutre\FastExcel\FastExcel;
 
 class EducationsController extends Controller
@@ -16,16 +15,16 @@ class EducationsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request  $request)
+    public function index(Request $request)
     {
         $term = $request->term;
-        if (empty($term)){
-            $educations = Education::orderby('name','ASC')->select('id','name')->get();
-        }else{
+        if (empty($term)) {
+            $educations = Education::orderby('name', 'ASC')->select('id', 'name')->get();
+        } else {
 
-            $educations = Education::where('name','like','%'.$request->search.'%')->orderby('name','ASC')->select('id','name')->get();
+            $educations = Education::where('name', 'like', '%' . $request->search . '%')->orderby('name', 'ASC')->select('id', 'name')->get();
         }
-       // dd($educations);
+        // dd($educations);
         return response()->json($educations);
 
     }
@@ -43,32 +42,32 @@ class EducationsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
         try {
-            $exist = Education::where('name',$request->name)->exists();
+            $exist = Education::where('name', $request->name)->exists();
 
-            if (!$exist){
+            if (!$exist) {
                 $education = Education::create(['name' => $request->name]);
                 if ($education) {
-                    return response()->json(['success'=>true , 'msg'=> 'Education added successfully'],200);
+                    return response()->json(['success' => true, 'msg' => 'Education added successfully'], 200);
                 }
-            }else{
-                return response()->json(['success'=>false , 'msg'=> 'Education already exist'],400);
+            } else {
+                return response()->json(['success' => false, 'msg' => 'Education already exist'], 400);
             }
 
-        }catch (\Exception $e){
-            return response()->json(['success'=>false , 'msg'=> 'Something went wrong'],500);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'msg' => 'Something went wrong'], 500);
         }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -79,7 +78,7 @@ class EducationsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -90,8 +89,8 @@ class EducationsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -102,37 +101,39 @@ class EducationsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         Education::destroy($id);
-        return response()->json(['msg'=>'Education Deleted successfully']);
+        return response()->json(['msg' => 'Education Deleted successfully']);
     }
 
     public function import(Request $request)
     {
-       try{
-           DB::beginTransaction();
-           $exist_edus = Education::all()->pluck('name')->toArray();
+        try {
+            DB::beginTransaction();
+            $exist_edus = Education::all()->pluck('name')->toArray();
+            $new_records= [];
+            if ($request->hasFile('import_file')) {
+                $file = $request->file('import_file');
+                $col = (new FastExcel())->import($file);
+                foreach ($col as $line){
+                    if (!in_array($line['CSC Education'], $exist_edus) && !empty($line['CSC Education'])) {
+                        array_push($new_records,['name'=>$line['CSC Education']]);
+                        array_push($exist_edus, $line['CSC Education']); // to avoid same excel duplicates
+                    }
+                }
 
-           if ($request->hasFile('import_file')) {
-               $file = $request->file('import_file');
-               $eds = (new FastExcel())->import($file,function ($line) use ($exist_edus){
-                   if (!in_array($line['CSC Education'],$exist_edus) && !empty($line['CSC Education'])){
-                       Education::create(['name'=> $line['CSC Education']]);
-                       array_push($exist_edus,$line['CSC Education']); // to avoid same excel duplicates
-                   }
-
-               });
-           }
+                Education::insert($new_records);
+            }
             DB::commit();
-           return response()->json(['msg'=>'Educations Created successfully']);
-       }catch (\Exception $e){
-           DB::rollBack();
-           return response()->json(['msg'=>'Something went wrong | '.$e->getMessage()],500);
-       }
+            return response()->json(['msg' => 'Educations Created successfully']);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['msg' => 'Something went wrong | ' . $e->getMessage()], 500);
+        }
 
     }
 }
